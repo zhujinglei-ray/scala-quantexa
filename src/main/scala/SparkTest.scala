734472922a22dc50c9597c9f3c6445f228991832
jinglei.zhu <jinglei.zhu@thehutgroup.com>
2020-02-23 00:54:31 +0000

    save to right place

--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -16,19 +16,33 @@ object SparkTest extends App {
 
   /* customized average for five day to fulfil the agg in window function */
   def customizedAverageFor5Day: Double => Double = { s => s / 5 }
+
   import org.apache.spark.sql.functions.udf
+
   val fiveDayAverage = udf(customizedAverageFor5Day)
 
   //Question 1: Calculate the total transaction value for all transactions for each day
-  //df.groupBy("transactionDay").sum("transactionAmount").orderBy("transactionDay").show(29)
-  //df.groupBy("transactionDay","accountId","category").avg("transactionAmount").orderBy("transactionDay","accountId").show(100)
+  val resultOfQOne = df.groupBy("transactionDay", "accountId", "category")
+    .avg("transactionAmount")
+    .orderBy("transactionDay", "accountId")
+
+  resultOfQOne.repartition(1).write.option("header", "true")
+    .csv("/Users/zhuj/scala-quantexa/spark-result/resultFfQuestion1")
+
 
   //Question 2: Calculate the average value of transactions per account for each type of transaction
-  //val newDF = df.groupBy("transactionDay","accountId").pivot("category").avg("transactionAmount")
-  //newDF.na.fill(0.0).orderBy("transactionDay","accountId").show()
+  val resultOfQTwo = df.groupBy("transactionDay", "accountId")
+    .pivot("category")
+    .avg("transactionAmount")
+    .na.fill(0.0)
+    .orderBy("transactionDay", "accountId")
+
+  resultOfQTwo.repartition(1).write.option("header", "true")
+    .csv("/Users/zhuj/scala-quantexa/spark-result/resultFfQuestion2")
+
 
   //Question 3: Calculate statistics for each account number for the previous five days of transactions
-  val newDF = df.groupBy("transactionDay", "accountId")
+  val dataFrameOfQThree = df.groupBy("transactionDay", "accountId")
     .pivot("category").sum("transactionAmount")
     .na.fill(0.0)
     .withColumn("sum_of_the_day", expr("AA+BB+CC+DD+EE+FF+GG"))
@@ -36,17 +50,15 @@ object SparkTest extends App {
 
   val listToRemove = List(1, 2, 3, 4, 5)
   val dayWindow = Window.partitionBy("accountId").orderBy("transactionDay").rangeBetween(-5, Window.currentRow - 1)
-  val result = newDF
+  val resultOfQThree = dataFrameOfQThree
     .withColumn("Maximum", max("sum_of_the_day").over(dayWindow))
     .withColumn("Average", sum("avg_daily").over(dayWindow))
     .withColumn("AA Total Value", sum("AA").over(dayWindow))
     .withColumn("CC Total Value", sum("CC").over(dayWindow))
     .withColumn("FF Total Value", sum("FF").over(dayWindow))
-    .drop("AA", "BB", "CC", "DD", "EE", "FF", "GG", "sum_of_the_day","avg_daily")
+    .drop("AA", "BB", "CC", "DD", "EE", "FF", "GG", "sum_of_the_day", "avg_daily")
     .filter(!(col("transactionDay") isin (listToRemove: _*)))
     .orderBy("transactionDay", "accountId")
 
-  Option(result.repartition(1).write.option("header", "true").csv("/Users/zhuj/scala-quantexa/out/result9"))
-
-
+  resultOfQThree.repartition(1).write.option("header", "true").csv("/Users/zhuj/scala-quantexa/spark-result/resultFfQuestion3")
 }
